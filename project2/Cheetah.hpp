cbbee2e82e3f145b2a305092afbe3626dd09e1d4
Paul Min <isimplyexcel@gmail.com>
2018-07-31 10:48:49 -0700

    change directory name

---
+++ project2/Cheetah.hpp
@@ -0,0 +1,19 @@
+/**********************************************************************************************
+** Program Name: Zoo
+** Author: Paul Min
+** Date: 07/13/18
+** Description: Cheetah.hpp is the Cheetah class specification file.
+************************************************************************************************/
+
+#ifndef CHEETAH_HPP
+#define CHEETAH_HPP
+#include "Animal.hpp"
+
+class Cheetah : public Animal{
+
+public:
+    Cheetah();  //  default constructor no arguments
+    Cheetah(int);
+};
+
+#endif
---
+++ project2/Tiger.hpp
@@ -0,0 +1,18 @@
+/**********************************************************************************************
+** Program Name: Zoo
+** Author: Paul Min
+** Date: 07/13/18
+** Description: Tiger.hpp is the Tiger class specification file. 
+************************************************************************************************/
+#ifndef TIGER_HPP
+#define TIGER_HPP
+#include "Animal.hpp"
+
+class Tiger : public Animal{
+
+public:	
+	Tiger();	//	Default constructor
+	Tiger(int);
+};
+
+#endif
